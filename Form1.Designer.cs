8e926b911b69a1c8d568814cbbb0f831783c382d
zenkichen <zenkichen@gmail.com>
2015-04-27 11:20:44 +0800

    遥测地址计算
    遥测地址计算
    无修改，用于测试github

---
+++ Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace calTeleAddr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTime1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtTime2 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.txtBlock = new System.Windows.Forms.TextBox();
+            this.txtHang = new System.Windows.Forms.TextBox();
+            this.txtKuai = new System.Windows.Forms.TextBox();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.txtBlock2 = new System.Windows.Forms.TextBox();
+            this.txtHang2 = new System.Windows.Forms.TextBox();
+            this.txtKuai2 = new System.Windows.Forms.TextBox();
+            this.btnCal = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtKuai);
+            this.groupBox1.Controls.Add(this.txtHang);
+            this.groupBox1.Controls.Add(this.txtBlock);
+            this.groupBox1.Controls.Add(this.txtTime1);
+            this.groupBox1.Controls.Add(this.label18);
+            this.groupBox1.Controls.Add(this.label17);
+            this.groupBox1.Controls.Add(this.label16);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label15);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(601, 124);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "参考时间";
+            // 
+            // txtTime1
+            // 
+            this.txtTime1.Location = new System.Drawing.Point(8, 33);
+            this.txtTime1.Name = "txtTime1";
+            this.txtTime1.Size = new System.Drawing.Size(227, 21);
+            this.txtTime1.TabIndex = 1;
+            this.txtTime1.Text = "2013-01-01 00:00:00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "yyyy-MM-dd hh:mm:ss";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtKuai2);
+            this.groupBox2.Controls.Add(this.txtHang2);
+            this.groupBox2.Controls.Add(this.txtTime2);
+            this.groupBox2.Controls.Add(this.txtBlock2);
+            this.groupBox2.Controls.Add(this.label22);
+            this.groupBox2.Controls.Add(this.label14);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label21);
+            this.groupBox2.Controls.Add(this.label19);
+            this.groupBox2.Controls.Add(this.label20);
+            this.groupBox2.Location = new System.Drawing.Point(12, 153);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(601, 124);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // txtTime2
+            // 
+            this.txtTime2.Location = new System.Drawing.Point(8, 33);
+            this.txtTime2.Name = "txtTime2";
+            this.txtTime2.Size = new System.Drawing.Size(227, 21);
+            this.txtTime2.TabIndex = 1;
+            this.txtTime2.Text = "2013-01-01 00:00:00";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(6, 17);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(53, 12);
+            this.label14.TabIndex = 0;
+            this.label14.Text = "待求时间";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(6, 70);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(53, 12);
+            this.label15.TabIndex = 0;
+            this.label15.Text = "参考地址";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(76, 87);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(17, 12);
+            this.label16.TabIndex = 0;
+            this.label16.Text = "片";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(165, 87);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(17, 12);
+            this.label17.TabIndex = 0;
+            this.label17.Text = "行";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(251, 87);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(17, 12);
+            this.label18.TabIndex = 0;
+            this.label18.Text = "块";
+            // 
+            // txtBlock
+            // 
+            this.txtBlock.Location = new System.Drawing.Point(8, 85);
+            this.txtBlock.Name = "txtBlock";
+            this.txtBlock.Size = new System.Drawing.Size(62, 21);
+            this.txtBlock.TabIndex = 1;
+            this.txtBlock.Text = "0";
+            // 
+            // txtHang
+            // 
+            this.txtHang.Location = new System.Drawing.Point(99, 84);
+            this.txtHang.Name = "txtHang";
+            this.txtHang.Size = new System.Drawing.Size(60, 21);
+            this.txtHang.TabIndex = 2;
+            this.txtHang.Text = "00";
+            // 
+            // txtKuai
+            // 
+            this.txtKuai.Location = new System.Drawing.Point(188, 84);
+            this.txtKuai.Name = "txtKuai";
+            this.txtKuai.Size = new System.Drawing.Size(57, 21);
+            this.txtKuai.TabIndex = 3;
+            this.txtKuai.Text = "00";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(6, 66);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(53, 12);
+            this.label19.TabIndex = 0;
+            this.label19.Text = "待求地址";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(76, 83);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(17, 12);
+            this.label20.TabIndex = 0;
+            this.label20.Text = "片";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(165, 83);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(17, 12);
+            this.label21.TabIndex = 0;
+            this.label21.Text = "行";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(251, 83);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(17, 12);
+            this.label22.TabIndex = 0;
+            this.label22.Text = "块";
+            // 
+            // txtBlock2
+            // 
+            this.txtBlock2.Location = new System.Drawing.Point(8, 81);
+            this.txtBlock2.Name = "txtBlock2";
+            this.txtBlock2.Size = new System.Drawing.Size(62, 21);
+            this.txtBlock2.TabIndex = 1;
+            // 
+            // txtHang2
+            // 
+            this.txtHang2.Location = new System.Drawing.Point(99, 80);
+            this.txtHang2.Name = "txtHang2";
+            this.txtHang2.Size = new System.Drawing.Size(60, 21);
+            this.txtHang2.TabIndex = 2;
+            // 
+            // txtKuai2
+            // 
+            this.txtKuai2.Location = new System.Drawing.Point(188, 80);
+            this.txtKuai2.Name = "txtKuai2";
+            this.txtKuai2.Size = new System.Drawing.Size(57, 21);
+            this.txtKuai2.TabIndex = 3;
+            // 
+            // btnCal
+            // 
+            this.btnCal.Location = new System.Drawing.Point(538, 296);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(75, 23);
+            this.btnCal.TabIndex = 5;
+            this.btnCal.Text = "计算";
+            this.btnCal.UseVisualStyleBackColor = true;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(251, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "yyyy-MM-dd hh:mm:ss";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 346);
+            this.Controls.Add(this.btnCal);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "遥测存储回算";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtKuai;
+        private System.Windows.Forms.TextBox txtHang;
+        private System.Windows.Forms.TextBox txtBlock;
+        private System.Windows.Forms.TextBox txtTime1;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtKuai2;
+        private System.Windows.Forms.TextBox txtHang2;
+        private System.Windows.Forms.TextBox txtTime2;
+        private System.Windows.Forms.TextBox txtBlock2;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Button btnCal;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace calTeleAddr
+{
+    public partial class Form1 : Form
+    {
+        private DateTime dt1, dt2;
+        private TimeSpan deltat;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCal_Click(object sender, EventArgs e)
+        {
+            long storeTime, block, hang, kuai, block2, hang2, kuai2;
+            long storeByte,haveByte;
+            if (txtTime1.Text == "")
+            {
+                MessageBox.Show("日期不能留空!", "确定");
+            }
+            if (txtTime2.Text == "")
+            {
+                MessageBox.Show("日期不能留空!", "确定");
+            }
+            if ((txtBlock.Text == "") && (txtHang.Text == "") && (txtKuai.Text == ""))
+            {
+                MessageBox.Show("地址不能留空!", "确定");
+            }
+            block = int.Parse(txtBlock.Text.ToString());
+            block2 = block;
+            hang = int.Parse(txtHang.Text.ToString());
+            kuai = int.Parse(txtKuai.Text.ToString());
+            dt1 = Convert.ToDateTime(txtTime1.Text.ToString());
+            dt2 = Convert.ToDateTime(txtTime2.Text.ToString());
+            if (dt2 > dt1)
+            {
+                deltat = dt2 - dt1;
+                storeTime = (long)deltat.TotalMinutes / 3;
+                storeByte = 768 * storeTime;
+                haveByte = hang * 2048 + kuai;
+                haveByte += storeByte;
+                kuai2 = haveByte % 2048;
+                hang2 = haveByte / 2048;
+                if (hang2 > 32768)
+                {
+                    hang2 -= 32768;
+                }
+            }
+            else
+            {
+                deltat = dt1 - dt2;
+                storeTime = (long)deltat.TotalMinutes / 3;
+                storeByte = 768 * storeTime;
+                haveByte = hang * 2048 + kuai;
+                if (haveByte < storeByte)
+                {
+                    haveByte = haveByte + 32768 * 2048;
+                }
+                haveByte = haveByte - storeByte;
+                kuai2 = haveByte % 2048;
+                hang2 = haveByte / 2048;
+            }
+            txtBlock2.Text = block2.ToString();
+            txtHang2.Text = hang2.ToString();
+            txtKuai2.Text = kuai2.ToString();
+        }
+    }
+}
